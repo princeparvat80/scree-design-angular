6aaa5993c5c269bfa91c4b6df1e20b85d304fe05
Deepali Sehgal <deepalis1@birlasoft.com>
2021-06-01 15:53:42 +0530

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CandidateComponent } from './candidate/candidate.component';
+import { BrowseJobsComponent } from './browse-jobs/browse-jobs.component';
+import { ManageJobsComponent } from './manage-jobs/manage-jobs.component';
+import { BrowseCategoriesComponent } from './browse-categories/browse-categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandidateComponent,
+    BrowseJobsComponent,
+    ManageJobsComponent,
+    BrowseCategoriesComponent
   ],
   imports: [
     BrowserModule,
