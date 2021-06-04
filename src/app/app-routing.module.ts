ff2a4d05e4b9690cd7d28c2552687e44d2532e78
Deepali Sehgal <deepalis1@birlasoft.com>
2021-06-04 14:06:13 +0530

    working on candidate page 30% is reamaining

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactSectionComponent } from './contact-section/contact-section.component';
 import { EmployerComponent } from './employer/employer.component';
+import { CandidateComponent } from './candidate/candidate.component';
 
 
 const routes: Routes = [
   {path:'employer',component:EmployerComponent},
-  {path:'Contact',component:ContactSectionComponent}
+  {path:'Contact',component:ContactSectionComponent},
+  {path:'candidate',component:CandidateComponent}
 ];
 
 @NgModule({
